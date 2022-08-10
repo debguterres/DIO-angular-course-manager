f896972c77aef6a5c507e31b60eb90a17b82d177
Debora Cervieri Guterres <debora.guterres@gmail.com>
2022-08-10 14:50:51 -0300

    Segregate application in modules

---
+++ src/app/shared/component/star.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { StarComponent } from "./star/star.component";
+
+@NgModule({
+    declarations: [
+        StarComponent
+    ],
+    exports: [
+        StarComponent
+    ]
+})
+export class StarModule {
+
+}
